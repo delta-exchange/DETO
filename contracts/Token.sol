1497c575f2cff0563bd6b7bc99a340020cd88b1d
Shraman <sraman.0330@gmail.com>
2020-10-21 00:40:17 +0530

    changes in vesting contract, burnable token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 
-contract Token is ERC20, ERC20Detailed, ERC20Mintable, ERC20Pausable, ERC20Capped {
+contract Token is ERC20, ERC20Detailed, ERC20Burnable, ERC20Mintable, ERC20Pausable, ERC20Capped {
     constructor(uint256 initialSupply) public ERC20Detailed("Delta Token", "DETO", 18) ERC20Capped(100*(10**6)*(10**18)) {
         _mint(msg.sender, initialSupply);
     }
--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -11,147 +11,140 @@ import "./Token.sol";
  */
 
 contract Vesting {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    // The vesting schedule is time-based (i.e. using block timestamps as opposed to e.g. block numbers), and is
-    // therefore sensitive to timestamp manipulation (which is something miners can do, to a certain degree). Therefore,
-    // it is recommended to avoid using short time durations (less than a minute). Typical vesting schemes, with a
-    // cliff period of a year and a duration of four years, are safe to use.
-    // solhint-disable not-rely-on-time
+  // The vesting schedule is time-based (i.e. using block timestamps as opposed to e.g. block numbers), and is
+  // therefore sensitive to timestamp manipulation (which is something miners can do, to a certain degree). Therefore,
+  // it is recommended to avoid using short time durations (less than a minute). Typical vesting schemes, with a
+  // cliff period of a year and a duration of four years, are safe to use.
+  // solhint-disable not-rely-on-time
 
-    modifier onlyOwner { require(msg.sender == owner); _; }
+  modifier onlyOwner { require(msg.sender == owner); _; }
 
-    address private owner;
-    Token private token;
+  address private owner;
+  Token private token;
 
-    // Durations and timestamps are expressed in UNIX time, the same units as block.timestamp.
-    struct Grant {
-      uint256 duration;
-      uint256 cliff;
+  // Durations and timestamps are expressed in UNIX time, the same units as block.timestamp.
+  struct Grant {
+    uint256 duration;
+    uint256 cliff;
+  }
+
+  mapping (address => uint256 ) private startTimes;
+  mapping (address => uint256) private amounts;
+  mapping (address => uint256) private releasedTokens;
+  mapping (address => string ) private beneficiaryGrant;
+  mapping (string => Grant) private grants;
+  uint256 private unreleasedTokens;
+
+  constructor(address tokenAddress) public {
+    token = Token(tokenAddress);
+    owner = msg.sender;
+  }
+
+  function addVestingGrant(string memory grantName, uint256 cliff, uint256 duration) onlyOwner public {
+    require(cliff >= 0, "TokenVesting: cliff is negative");
+    require(duration >= 0, "TokenVesting: duration is negative");
+    Grant memory grant = Grant({
+      duration: duration,
+      cliff: cliff
+    });
+
+    grants[grantName] = grant;
+  }
+
+  function vestTokens(address beneficiary, uint256 amount, string memory grantName, uint256 startTime) onlyOwner public  {
+    require(beneficiary != address(0), "TokenVesting: beneficiary is the zero address");
+
+    Grant memory grant = grants[grantName];
+    require(startTime.add(grant.duration) > block.timestamp, "TokenVesting: final time is before current time");
+
+    unreleasedTokens = unreleasedTokens.add(amount);
+    uint256 contractTokens = token.balanceOf(address(this));
+    require(unreleasedTokens <= contractTokens, "TokenVesting: Total amount allocated should be less than tokens in contract");
+
+    startTimes[beneficiary] = startTime;
+    amounts[beneficiary] = amount;
+    releasedTokens[beneficiary] = 0;
+    beneficiaryGrant[beneficiary] = grantName;
+  }
+
+  /**
+    * @return the start time of the token vesting.
+    */
+  function startTime(address beneficiary) public view returns (uint256) {
+    return startTimes[beneficiary];
+  }
+
+  /**
+    * @return the cliff time of the token vesting.
+    */
+  function cliff(address beneficiary) public view returns (uint256) {
+    string memory grantName = beneficiaryGrant[beneficiary];
+    return grants[grantName].cliff;
+  }
+
+  /**
+    * @return the duration of the token vesting.
+    */
+  function duration(address beneficiary) public view returns (uint256) {
+    string memory grantName = beneficiaryGrant[beneficiary];
+    return grants[grantName].duration;
+  }
+
+  /**
+    * @return the amount of the tokens alloted .
+    */
+  function amount(address beneficiary) public view returns (uint256) {
+    return amounts[beneficiary];
+  }
+
+  /**
+    * @return the amount of the token released.
+    */
+  function releasedAmount(address beneficiary) public view returns (uint256) {
+    return releasedTokens[beneficiary];
+  }
+
+  /**
+    * @notice Transfers vested tokens to beneficiary.
+    */
+  function release(address beneficiary) public {
+    require(msg.sender == owner || msg.sender == beneficiary, "TokenVesting: Tokens can be released only by owner or beneficiary");
+
+    uint256 unreleased = releasableAmount(beneficiary);
+
+    require(unreleased > 0, "TokenVesting: no tokens are due");
+
+    releasedTokens[beneficiary] = releasedTokens[beneficiary].add(unreleased);
+
+    unreleasedTokens = unreleasedTokens.sub(unreleased);
+    token.transfer(beneficiary, unreleased);
+  }
+
+  /**
+    * @dev Calculates the amount that has already vested but hasn't been released yet.
+    */
+  function releasableAmount(address beneficiary) public view returns (uint256) {
+      return vestedAmount(beneficiary).sub(releasedTokens[beneficiary]);
+  }
+
+  /**
+    * @dev Calculates the amount that has already vested.
+    */
+  function vestedAmount(address beneficiary) public view returns (uint256) {
+    uint256 totalAmount = amounts[beneficiary];
+    string memory grantName = beneficiaryGrant[beneficiary];
+    Grant memory grant = grants[grantName];
+    uint256 start = startTimes[beneficiary];
+
+    if (block.timestamp < start.add(grant.cliff)) {
+        return 0;
+    } else if (block.timestamp >= start.add(grant.cliff).add(grant.duration)) {
+        return totalAmount;
+    } else {
+        return totalAmount.mul(block.timestamp.sub(start.add(grant.cliff))).div(grant.duration);
     }
-
-    mapping (address => uint256 ) private startTimes;
-    mapping (address => uint256) private amounts;
-    mapping (address => uint256) private releasedTokens;
-    mapping (address => string ) private beneficiaryGrant;
-    mapping (string => Grant) private grants;
-
-
-    // BELOW to be removed after finalising contract
-    // uint256 public birthdate;
-    // function set(uint256 _birthdate) public {
-    //     birthdate = _birthdate;
-    // }
-
-    // function get() public view returns (uint256) {
-    //     return birthdate;
-    // }
-
-    // function getBlock() public view returns (uint256) {
-    //     return block.timestamp;
-    // }
-    // -------
-
-    constructor(address tokenAddress) public {
-      token = Token(tokenAddress);
-      owner = msg.sender;
-    }
-
-    function addVestingGrant(string memory grantName, uint256 cliff, uint256 duration) onlyOwner public {
-      require(cliff <= duration, "TokenVesting: cliff is longer than duration");
-      require(duration > 0, "TokenVesting: duration is 0");
-      Grant memory grant = Grant({
-        duration: duration,
-        cliff: cliff
-      });
-
-      grants[grantName] = grant;
-    }
-
-    function vestTokens(address beneficiary, uint256 amount, string memory grantName, uint256 startTime) onlyOwner public  {
-      require(beneficiary != address(0), "TokenVesting: beneficiary is the zero address");
-      Grant memory grant = grants[grantName];
-      require(startTime.add(grant.duration) > block.timestamp, "TokenVesting: final time is before current time");
-
-      startTimes[beneficiary] = startTime;
-      amounts[beneficiary] = amount;
-      releasedTokens[beneficiary] = 0;
-      beneficiaryGrant[beneficiary] = grantName;
-    }
-
-    /**
-     * @return the start time of the token vesting.
-     */
-    function startTime(address beneficiary) public view returns (uint256) {
-      return startTimes[beneficiary];
-    }
-
-    /**
-     * @return the cliff time of the token vesting.
-     */
-    function cliff(address beneficiary) public view returns (uint256) {
-      string memory grantName = beneficiaryGrant[beneficiary];
-      return grants[grantName].cliff;
-    }
-
-    /**
-     * @return the duration of the token vesting.
-     */
-    function duration(address beneficiary) public view returns (uint256) {
-      string memory grantName = beneficiaryGrant[beneficiary];
-      return grants[grantName].duration;
-    }
-
-    /**
-     * @return the amount of the tokens alloted .
-     */
-    function amount(address beneficiary) public view returns (uint256) {
-      return amounts[beneficiary];
-    }
-
-    /**
-     * @return the amount of the token released.
-     */
-    function releasedAmount(address beneficiary) public view returns (uint256) {
-      return releasedTokens[beneficiary];
-    }
-
-    /**
-     * @notice Transfers vested tokens to beneficiary.
-     */
-    function release(address beneficiary) public onlyOwner {
-        uint256 unreleased = releasableAmount(beneficiary);
-
-        require(unreleased > 0, "TokenVesting: no tokens are due");
-
-        releasedTokens[beneficiary] = releasedTokens[beneficiary].add(unreleased);
-
-        token.mint(beneficiary, unreleased);
-    }
-
-    /**
-     * @dev Calculates the amount that has already vested but hasn't been released yet.
-     */
-    function releasableAmount(address beneficiary) public view returns (uint256) {
-        return vestedAmount(beneficiary).sub(releasedTokens[beneficiary]);
-    }
-
-    /**
-     * @dev Calculates the amount that has already vested.
-     */
-    function vestedAmount(address beneficiary) public view returns (uint256) {
-        uint256 totalAmount = amounts[beneficiary];
-        string memory grantName = beneficiaryGrant[beneficiary];
-        Grant memory grant = grants[grantName];
-        uint256 start = startTimes[beneficiary];
-
-        if (block.timestamp < start.add(grant.cliff)) {
-            return 0;
-        } else if (block.timestamp >= start.add(grant.duration)) {
-            return totalAmount;
-        } else {
-            return totalAmount.mul(block.timestamp.sub(start)).div(grant.duration);
-        }
-    }
+  }
 
 }
